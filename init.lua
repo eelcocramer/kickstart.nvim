4b125e9b21743b651b8e8d470cf7984f46700a55
Eelco Cramer <eelco@servicelab.org>
2023-11-18 12:17:47 +0100

    Adds detour removes goformat on save

--- init.lua
+++ init.lua
@@ -647,14 +647,14 @@ vim.opt.cursorline = true
 
 -- Go specific config
 -- Run gofmt + goimport on save
-local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    require('go.format').goimport()
-  end,
-  group = format_sync_grp,
-})
+-- local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*.go",
+--   callback = function()
+--     require('go.format').goimport()
+--   end,
+--   group = format_sync_grp,
+-- })
 
 
 -- The line beneath this is called `modeline`. See `:help modeline`
