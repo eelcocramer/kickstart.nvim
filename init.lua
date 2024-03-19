a768fa38f4df8b4a7b03adffb848a5f8bc3c988c
Eelco Cramer <eelco@servicelab.org>
2024-03-19 20:29:05 +0100

    Fixes issue

--- init.lua
+++ init.lua
@@ -803,17 +803,17 @@ require('lazy').setup({
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
       --  and try some other statusline plugin
---      local statusline = require 'mini.statusline'
---      -- set use_icons to true if you have a Nerd Font
---      statusline.setup { use_icons = vim.g.have_nerd_font }
---
---      -- You can configure sections in the statusline by overriding their
---      -- default behavior. For example, here we set the section for
---      -- cursor location to LINE:COLUMN
---      ---@diagnostic disable-next-line: duplicate-set-field
---      statusline.section_location = function()
---        return '%2l:%-2v'
---      end
+      --      local statusline = require 'mini.statusline'
+      --      -- set use_icons to true if you have a Nerd Font
+      --      statusline.setup { use_icons = vim.g.have_nerd_font }
+      --
+      --      -- You can configure sections in the statusline by overriding their
+      --      -- default behavior. For example, here we set the section for
+      --      -- cursor location to LINE:COLUMN
+      --      ---@diagnostic disable-next-line: duplicate-set-field
+      --      statusline.section_location = function()
+      --        return '%2l:%-2v'
+      --      end
 
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
@@ -839,7 +839,7 @@ require('lazy').setup({
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
       ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts) {
+      require('nvim-treesitter.configs').setup {
         ensure_installed = { 'go', 'python', 'rust', 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc', 'typescript' },
         -- Autoinstall languages that are not installed
         auto_install = true,
