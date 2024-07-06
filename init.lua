0c54647bede9c82c14e980be28056a5b3d7cb0cd
Eelco Cramer <eelco@servicelab.org>
2024-07-06 14:36:43 +0200

    Updates neodev to lazydev

--- init.lua
+++ init.lua
@@ -454,9 +454,19 @@ require('lazy').setup({
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
-      -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+      -- `lazydev configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
+      {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+          library = {
+            -- Load luvit types when the `vim.uv` word is found
+            { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+          },
+        },
+      },
+      { 'Bilal2453/luvit-meta', lazy = true },
     },
     config = function()
       -- Brief aside: **What is LSP?**
