5ffae03163feb20ea792ed3afe4a04a36b2e5d88
Eelco Cramer <eelco@servicelab.org>
2024-07-06 14:25:23 +0200

    Disables default diagnostics

--- lua/custom/plugins/tiny-inline-diagnostic.lua
+++ lua/custom/plugins/tiny-inline-diagnostic.lua
@@ -3,11 +3,12 @@ return {
   event = 'VeryLazy',
   config = function()
     vim.opt.updatetime = 100
+    vim.diagnostic.config { virtual_text = false }
     require('tiny-inline-diagnostic').setup()
 
     -- disable inline lsp lines
-    vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      virtual_text = false,
-    })
+    --vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- virtual_text = false,
+    --})
   end,
 }
