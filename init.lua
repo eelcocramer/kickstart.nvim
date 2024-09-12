d218c0132d17176664b4d4189ea0779ad4a85819
Eelco Cramer <eelco@servicelab.org>
2024-09-12 15:26:24 +0200

    C formatting

--- init.lua
+++ init.lua
@@ -702,6 +702,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         xml = { 'xmlformat' },
+        c = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
